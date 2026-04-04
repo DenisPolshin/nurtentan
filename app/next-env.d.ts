342c84a840fb25545bf91f796e4d275a4274bce6
DenisPolshin <113425051+DenisPolshin@users.noreply.github.com>
2026-04-04 14:38:41 +0200

    Auto-deploy update

--- app/next-env.d.ts
+++ app/next-env.d.ts
@@ -4,16 +4,3 @@ import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-
-import "next-auth";
-
-declare module "next-auth" {
-  interface User {
-    role?: string;
-  }
-  interface Session {
-    user: User & {
-      role?: string;
-    };
-  }
-}
